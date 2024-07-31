f8934d39f103cdc18448f04372a22251e1386506
tao.xing <tao.xing@kyligence.com>
2024-07-31 14:10:13 +0800

    add scripts v2

--- scripts/sendrsa.sh
+++ scripts/sendrsa.sh
@@ -4,7 +4,7 @@ while read line
   do
     user="root"
     ip=`echo $line | cut -d " " -f 1`
-    passwd="DevOps@2024#!@"
+    passwd="hadoop"
     expect <<EOF
       set timeout 10
       spawn ssh-copy-id -i /root/.ssh/id_rsa.pub $user@$ip
@@ -14,4 +14,4 @@ while read line
       }
       expect "password" { send "$passwd\n" }
 EOF
-  done < /root/scripts/hosts/idc-hadoop.txt
+  done < /root/scripts/hosts/test.txt
